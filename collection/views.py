de6114afce8eb44faa240153f2d223fa0e0ebab3
Ubuntu <ubuntu@ip-172-31-80-37.ec2.internal>
2019-10-24 11:53:29 +0000

    styled collection view

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.utils import timezone
 from artifacts.models import Artifact
 from auctions.models import Auction
 from bid.models import Bids
@@ -6,9 +7,19 @@ from bid.models import Bids
 def view_collection(request):
     """A view that renders the collection contents page"""
     collection = request.session.get('collection')
+    
+    auctions = Auction.objects.filter(end_date__lte=timezone.now())
+    artifacts_won = {}
+    for auction in auctions:
+        bids = Bids.objects.filter(auction=auction)
+        highest_bid = bids.order_by('-bid_amount')[0].bid_amount
+        highest_bidder = bids.order_by('-bid_amount')[0].bidder
+        if highest_bidder==request.user:
+            artifacts_won[auction.artifact.id]={ 'artifact' : auction.artifact, 'bid' : highest_bid }
+    
     artifacts_owned = Artifact.objects.filter(owner=request.user)
 
-    return render(request, "collection.html", { "artifacts_owned" : artifacts_owned })
+    return render(request, "collection.html", { "artifacts_owned" : artifacts_owned, "artifacts_won" : artifacts_won })
 
 def add_to_collection(request, id, buy_now):
     """
@@ -24,7 +35,7 @@ def add_to_collection(request, id, buy_now):
     if int(buy_now)==1:
         price = artifact.buy_now_price
     else:
-        price = last_bid
+        price = float(last_bid)
     
     if id not in collection:
         collection[id] = collection.get(id, price)
