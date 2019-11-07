60a2bd3693f7a1a90c0d71a293c6867089e3b6f9
Ubuntu <ubuntu@ip-172-31-80-37.ec2.internal>
2019-11-07 09:59:51 +0000

    udpated registration page for better interation with profile picture

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {    
     $('#id_profile_picture').change(function(e){
         var fileName = e.target.files[0].name;
-        $("#profile-picture-selected").text("File selected: "+fileName);
+        var file = e.target.files[0];
+        console.log("file="+$(this).val())
+        $("#profile-picture-selected").html("File selected: "+fileName+"<br>Click 'Update' to upload image.");
+        $('#id_profile--clear-image-selection, #id_profile--clear-image-selection-label').show();
     });
+    
+    $("#id_profile--clear-image-selection").change(function() {
+        $('#id_profile_picture').val('');  
+        $("#profile-picture-selected").html("");
+        $('#id_profile--clear-image-selection, #id_profile--clear-image-selection-label').hide();
+        $(this).prop("checked", false)
+    })
     
     $('#id_remain_anonymous').change(function(e){
         if($('input[name="remain_anonymous"]').prop('checked')) {
