879fe982eed8e0c0ee372a860d3ef0b6aad97440
Ubuntu <ubuntu@ip-172-31-80-37.ec2.internal>
2019-11-11 22:48:12 +0000

    restyled 'back' button

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@
         var titleWidth = $(".index--title-image").width();
         var titleHeight = titleWidth * .7331;
         $(".index--title-image").css("height", titleHeight + "px")
-        if($(window).width()>=1280) {
+        if($(window).width()>=992) {
             $(".index--title-image").css("width", "92%");
             $(".index--title-image").css("margin", "5vh auto")   
         }
